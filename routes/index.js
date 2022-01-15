6bf0ffcf3b414b208a8886e9937257f6f18c8048
Disha Bhardwaj <disha2.bhardwaj@gmail.com>
2022-01-15 12:21:14 +0530

    Added basic crud functionality

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,32 @@ router.get('/',async(req,res)=>{
     res.send(items);
 })
 
+router.post('/',async(req,res)=>{
+    const {ItemName, Price , TotalQty } = req.body;
+    const newItem = new Inventory({
+        ItemName,
+        Price,
+        TotalQty
+    }) 
+    await newItem.save()
+    .then(()=>res.send("Item Added"))
+})
+router.get('/:id',async(req,res)=>{
+    const { id } = req.params;
+    const item = await Inventory.findById(id);
+    res.send(item);
+})
+
+router.put('/edit/:id',async(req,res)=>{
+    const { id } = req.params;
+    const {ItemName, TotalQty, Price } = req.body;
+    await Inventory.findByIdAndUpdate(id,{TotalQty,Price,ItemName})
+    .then(()=>res.send('Item updated'));
+})
+router.delete('/delete/:id',async(req,res)=>{
+    const { id } = req.params;
+    Inventory.findByIdAndDelete(id)
+    .then(()=>res.send("Item deleted"));
+})
+
 module.exports = router;
